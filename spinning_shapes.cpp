14d0f71dd54fa6616e68d00b8e96a27dcd9a147a
JFelipeDR <j.felipe.d.r.10@gmail.com>
2022-12-04 13:50:01 -0500

    revision 1.0

--- spinning_shapes.cpp
+++ spinning_shapes.cpp
@@ -1,28 +1,45 @@
 #include <iostream>
 #include <stdlib.h>
-#include<ctime>
+#include <ctime>
 
 //delay function
 void delay(int secs) {
   for(int i = (time(NULL) + secs); time(NULL) != i; time(NULL));
 }
 
+//clear screen no matter the system
+void clear_s() {
+  #ifdef _WIN32
+    system("cls");
+  #else
+    system("clear");
+  #endif
+}
+
+
 int main()
 {
     int sec = 1;
+    char shape_select;
     std::cout << "\n";
     std::cout << "\t------------------------------------------------------------" << "\n";
-    std::cout << "\t-------------Program to render a shape spinning-------------" << "\n";
+    std::cout << "\t------------ Program to render a shape spinning ------------" << "\n";
     std::cout << "\t------------------------------------------------------------" << std::endl;
     delay(sec);
-    system("cls");
 
-    std::cout <<"|   |  |"<<"\n";
-    std::cout <<"|===|  |"<<"\n";
-    std::cout <<"|j  |  |"<<"\n";
-    std::cout <<"| g |  |"<<"\n";
-    std::cout <<"|  s| /"<<"\n";
-    std::cout <<"|===|/"<<"\n";
-    std::cout <<"'---'"<<"\n";
-    delay(sec);
+    do {
+        clear_s();
+        std::cout << "\t------------------------------------------------------------" << "\n";
+        std::cout << "\t--------------------- Select the shape ---------------------" << "\n";
+        std::cout << "\t------------------------------------------------------------" << "\n";
+        std::cout << "\n\n\tC --> Circle" << "\n";
+        std::cout << "\tS --> Square" << "\n";
+
+        std::cin >> shape_select;
+
+    } while (shape_select != 'C' && shape_select != 'S' && shape_select != 'c' && shape_select != 's');
+
+    
+    
+    return 0;
 }
